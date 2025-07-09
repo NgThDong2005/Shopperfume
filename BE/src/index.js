81089335781f9955a1f89d619fb283f1b8703ce2
NgThDOng2005 <nguyenthanhdongtn0412@gmail.com>
2025-07-09 11:46:49 +0700

    create dashboard page

---
+++ BE/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.set('views', path.join(__dirname, '../../FE/src/views'));
+
+app.get('/dashboard', (req, res) => {
+  res.render('admin/pages/dashboard/index');
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
