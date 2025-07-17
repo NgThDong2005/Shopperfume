90ba4e71474995732671563eeeac45f129df4606
NgThDOng2005 <nguyenthanhdongtn0412@gmail.com>
2025-07-17 20:19:42 +0700

    update

--- BE/config/database.js
+++ BE/config/database.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql2';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -8,48 +8,15 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-console.log("Database config:", {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-connection.connect((err) => {
-  if (err) {
-    return console.error('Connection failed:', err.message);
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    dialect: 'mysql'
   }
-  console.log('Connected to Railway MySQL!');
+);
 
-  connection.query('SELECT NOW()', (err, results) => {
-    if (err) throw err;
-    console.log('Current time:', results[0]);
-    connection.end();
-  });
-});
-
-
-connection.query('SHOW TABLES', (err, results) => {
-  if (err) throw err;
-
-  const tableKey = Object.keys(results[0])[0]; 
-  const tableNames = results.map(row => row[tableKey]);
-
-  console.log('Tables:', tableNames);
-
-  tableNames.forEach((table) => {
-    connection.query(`SELECT * FROM \`${table}\``, (err, rows) => {
-      if (err) throw err;
-      console.log(`Data in table [${table}]:`, rows);
-    });
-  });
-});
+export default sequelize;
