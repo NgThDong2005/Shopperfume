b86487b1d228525bec27f7484764ca81d41eb811
Trần Trọng Hưng <tthung23@clc.fitus.edu.vn>
2025-08-28 23:32:16 +0700

    Change import to use namespace for product controller

--- BE/routes/client/product.route.js
+++ BE/routes/client/product.route.js
@@ -1,7 +1,7 @@
 import express from "express"
 const router = express.Router()
 
-import controller from "../../controllers/client/product.controller.js";
+import * as controller from "../../controllers/client/product.controller.js";
 router.get("/search", controller.search);
 router.get("/:slug", controller.index);
 
